15bb62f95a5dde50d6c16df2c79d75a2d1ed5ae4
Scott Peshak <scott.peshak@meredith.com>
2016-02-29 15:50:39 -0600

    Customize system config to be to my liking

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="$PATH:/usr/local/git/bin:/usr/local/bin:/opt/local/bin:/opt/local/sbin:/opt/local/bin:/opt/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/X11/bin:$HOME/bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
