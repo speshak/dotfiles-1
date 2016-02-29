f98cae7d7124a85af0b1ba4165034db659e154cc
Scott Peshak <scott.peshak@meredith.com>
2016-02-29 10:30:47 -0600

    Add accumulated aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,10 @@
 alias reload!='. ~/.zshrc'
+
+alias cppwd="pwd | pbcopy"
+alias cal="gcal -q US_IA"
+alias cat="ccat"
+alias http="http --verify=no"
+alias weather="curl wttr.in"
+
+alias -s rb=vim
+alias -s md=vim
