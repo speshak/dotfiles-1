04e7f2aa19bbe99c602e6f4069dc76678af561b4
Scott Peshak <scott.peshak@meredith.com>
2018-12-19 10:26:22 -0600

    Only activate RVM stuff on hosts with rvm

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,6 +1,8 @@
 # Enable RVM
-export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
-source ~/.rvm/scripts/rvm
+if [ -e "$HOME/.rvm" ]; then
+  export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+  source ~/.rvm/scripts/rvm
+fi
 
 # Enable virtualenv
 if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
